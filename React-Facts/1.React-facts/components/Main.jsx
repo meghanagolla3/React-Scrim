7e2d46cecac33dd9faa4cf007991861fe8f377eb
MeghanaGolla <meghanagolla33@gmail.com>
2025-09-08 23:33:27 +0530

    Completed First Page

---
+++ React-Facts/1.React-facts/components/Main.jsx
@@ -0,0 +1,19 @@
+function Main() {
+  return (
+    <>
+      <main>
+        <img src="src/assets/react.svg" alt="" className="img-bg"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li className="list-item">Was first released in 2013</li>
+          <li className="list-item">Was originally created by Jordan Walke</li>
+          <li className="list-item">It was Composable</li>
+          <li className="list-item">Declarative</li>
+          <li className="list-item">Less magic</li>
+        </ul>
+
+      </main>
+    </>
+  )
+}
+export default Main;
---
+++ React-Facts/1.React-facts/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <>
+      <header>
+        <nav className="nav">
+          <img src="src/assets/react.svg" alt="" />
+          <span className="nav-item" >ReactFacts</span>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default Navbar;
